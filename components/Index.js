60f9fd0d34a4992378fc1e7e7185bf53b7df3c9d
David O'Donohue <virtuspsychology@gmail.com>
2022-10-04 17:15:38 +1100

    commit

---
+++ components/Index.js
@@ -0,0 +1,8 @@
+const Index = Vue.component('index', {
+    template: `<div>
+    <div class="lander"><h3>Flexible Psyche</h3></div>
+    Rosalie Poesiat is a blah blah blah.<br><br>
+    More cool stuff<br><br>
+    Awesome!
+    </div>`
+});
---
+++ components/Invalid.js
@@ -0,0 +1,5 @@
+const Invalid = Vue.component('invalid', {
+    template: `<div>
+<h3>This page does not exist!</h3><br>
+    </div>`
+});
